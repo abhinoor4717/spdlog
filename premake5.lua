e66ef428d5f583ddbac7902a2e811dfb2f6e9efe
abhinoor4717 <117553206+abhinoor4717@users.noreply.github.com>
2025-07-18 13:08:44 -0400

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "spdlog"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    includedirs {
+        "include"
+    }
+
+    files {
+        "include/**.h",
+        "include/**.tcc",
+        "src/**.cpp"
+    }
+
+    defines {
+        "SPDLOG_COMPILED_LIB"
+    }
+
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
